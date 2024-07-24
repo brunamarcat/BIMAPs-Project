f8bafb4ffe5a4ad3f80a3e915424483a3eee297f
brunamarcat <150627637+brunamarcat@users.noreply.github.com>
2024-07-24 23:10:40 +0200

    bug losses fixed

--- TripletLoses.py
+++ TripletLoses.py
@@ -1,17 +1,8 @@
 
 import tensorflow as tf
-from tensorflow.keras.losses import Loss, MSE
+from tensorflow.keras.losses import Loss, MSE, MeanSquaredError
 import segmentation_models as sm
 
-# l1=0.2
-# l2=0.2
-# margin1=0
-# margin2=0.2
-
-# l1=0
-# l2=0
-# margin1=0
-# margin2=0
 
 class Triplet_loss_function_dendrite(Loss):
     def __init__(self, l = 0, margin = 0):
@@ -20,9 +11,16 @@ class Triplet_loss_function_dendrite(Loss):
         self.margin = margin
 
     def call(self, y_true, y_pred):
-        original_loss = sm.losses.dice_loss(y_true, y_pred)
         y_p_reshaped = tf.reshape(y_pred, (-1, 3, 128, 128, 1))
-        triplet_loss=tf.math.maximum(tf.convert_to_tensor(0,dtype='float32'), tf.reduce_mean(MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,1,:,:])-MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,2,:,:])+self.margin))
+        y_t_reshaped = tf.reshape(y_true, (-1, 3, 128, 128, 1))
+        original_loss = sm.losses.dice_loss(y_t_reshaped[:,0,:,:,:], y_p_reshaped[:,0,:,:,:])
+        mask = tf.not_equal(tf.squeeze(y_t_reshaped[:,0,:,:]),tf.squeeze(y_t_reshaped[:,2,:,:]))
+        Aux = MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,2,:,:])
+        Aux =tf.where(mask, Aux, tf.zeros_like(Aux))
+        triplet_loss=tf.math.maximum(tf.convert_to_tensor(0,dtype='float32'), 
+                    tf.reduce_mean(MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,1,:,:])) -
+                    tf.reduce_mean(Aux)
+                    +self.margin)
         return original_loss+ self.l*triplet_loss
 
 
@@ -33,7 +31,14 @@ class Triplet_loss_function_spine(Loss):
         self.margin = margin
 
     def call(self, y_true, y_pred):
-        original_loss = MSE(y_true, y_pred)
+        y_t_reshaped = tf.reshape(y_true, (-1, 3, 128, 128, 1))
         y_p_reshaped = tf.reshape(y_pred, (-1, 3, 128, 128, 1))
-        triplet_loss=tf.math.maximum(tf.convert_to_tensor(0,dtype='float32'), tf.reduce_mean(MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,1,:,:])-MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,2,:,:])+self.margin))
-        return tf.reduce_mean(original_loss, axis=-1)+self.l*triplet_loss
+        original_loss = MSE(y_t_reshaped[:,0,:,:,:], y_p_reshaped[:,0,:,:,:])
+        mask = tf.not_equal(tf.squeeze(y_t_reshaped[:,0,:,:]),tf.squeeze(y_t_reshaped[:,2,:,:]))
+        Aux = MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,2,:,:])
+        Aux =tf.where(mask, Aux, tf.zeros_like(Aux))
+        triplet_loss=tf.math.maximum(tf.convert_to_tensor(0,dtype='float32'), 
+                    tf.reduce_mean(MSE(y_p_reshaped[:,0,:,:], y_p_reshaped[:,1,:,:])) -
+                    tf.reduce_mean(Aux)
+                    +self.margin)
+        return tf.reduce_mean(original_loss)+self.l*triplet_loss        
